951dcf772b3cdcd54e671f05e5bc90584e019a58
AraelHidalgo <arael.hidalgo54@unach.mx>
2025-05-18 13:33:54 -0600

    Add documentation for environment variables

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './', // Use relative paths for assets
     optimizeDeps: {
         noDiscovery: true
     },
